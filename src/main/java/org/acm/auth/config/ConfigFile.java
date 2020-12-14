d98e79caaf2753eaae7ab07317721f859306c7a6
Anastasios Papadopoulos <apapadoi@csd.auth.gr>
2020-12-14 17:01:34 +0200

    Added comments to ConfigFile.java and ConfigKey.java.

--- src/main/java/org/acm/auth/config/ConfigFile.java
+++ src/main/java/org/acm/auth/config/ConfigFile.java
@@ -7,35 +7,59 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
+/**
+ * Handles a config file.
+ */
 public class ConfigFile {
     private final ConfigKey[] requiredKeys;
     private final File cfgFile;
     private JSONObject json;
 
+    /**
+     * Construct a config file handler.
+     * @param pathName The path of the config file as {@code String}
+     * @throws IOException If an error occurs e.g. when reading the file
+     */
     public ConfigFile(String pathName) throws IOException {
+        // set the required keys that we need from the config file
         this.requiredKeys = new ConfigKey[] { ConfigKey.TOKEN };
+        // bind a File object to the config file using the incoming pathName
         this.cfgFile = new File(pathName);
         if (this.cfgFile.createNewFile()) {
             // we just created the file,
             // let's populate it
             populateFile();
+            // ask from the user to add the proper config keys to the config file
             System.out.println("Config file generated! Fill your credentials.");
             System.exit(0);
         } else {
-            // createNewFile() returns false
-            // when the file already exists
+            // createNewFile() returns false when the file already exists
+            // read the file
             readFile();
+            // and then verify it
             verifyFile();
         }
     }
 
+    /**
+     * Reads the config file and saves the key-value pairs to the private JSON Object attribute
+     * @throws IOException If an error occurs when reading the file
+     */
     private void readFile() throws IOException {
+        // read the lines of the config file and save each line as a list element
         List<String> lines = Files.readAllLines(cfgFile.toPath());
+        // convert the elements of the list to a single String
         String fileContent = String.join("", lines);
+        // and then convert this single String to a JSON Object
         json = new JSONObject(fileContent);
     }
 
-    private void verifyFile() {
+    /**
+     * Verifies the config file.
+     * @throws IllegalStateException if a required key-value pair is missing
+     */
+    private void verifyFile() throws IllegalStateException{
+        // iterate through the required keys and if a required key-value pair is missing throw an IllegalStateException
         for (ConfigKey key : this.requiredKeys) {
             if (!json.has(key.getKey())) {
                 throw new IllegalStateException("Missing required key: " + key.getKey());
@@ -43,26 +67,39 @@ public class ConfigFile {
         }
     }
 
+    /**
+     * Returns the value of a config key
+     * @param cfgKey the key that we want its value as {@code ConfigKey}
+     * @return the requested value as {@code String}
+     */
     public String getValue(ConfigKey cfgKey) {
         String value;
         if (json.has(cfgKey.getKey())) {
+            // the config file has the requested key so get its value using the json attribute
             value = json.getString(cfgKey.getKey());
-            if (value.equals("")) {
+            if (value.equals("")) { // the value is the empty String( the user did not fill his own value ) so get the
+                                    // default value of the requested key
                 value = cfgKey.getDefaultValue();
             }
         } else {
-            // config key missing from the file
-            // get its default value
+            // config key missing from the file so get its default value
             value = cfgKey.getDefaultValue();
         }
         return value;
     }
 
+    /**
+     * Fills the config file with the required key-value pairs so the user can go and fill his own credentials.
+     * At the start, default values are used for each key-value pair
+     * @throws IOException if an error occurs when writing to the file
+     */
     private void populateFile() throws IOException {
+        // create a JSON Object and add to it the required key-value pairs
         JSONObject json = new JSONObject();
         for (ConfigKey cfgKey : ConfigKey.values()) {
             json.put(cfgKey.getKey(), cfgKey.getDefaultValue());
         }
+        // when finished, write the key-value pairs to the config file using the path of the attribute cfgFile
         Files.write(this.cfgFile.toPath(), json.toString(2).getBytes());
     }
 }
--- src/main/java/org/acm/auth/config/ConfigKey.java
+++ src/main/java/org/acm/auth/config/ConfigKey.java
@@ -1,5 +1,8 @@
 package org.acm.auth.config;
 
+/**
+ * Declares config keys
+ */
 public enum ConfigKey {
     TOKEN("token", ""),
     PREFIX("prefix", "-"),
@@ -9,15 +12,28 @@ public enum ConfigKey {
     private final String key;
     private final String defaultValue;
 
+    /**
+     * Construct a config key with the incoming key and defaultValue
+     * @param key the config key as {@code String}
+     * @param defaultValue the key's default value as {@code String}
+     */
     ConfigKey(String key, String defaultValue) {
         this.key = key;
         this.defaultValue = defaultValue;
     }
 
+    /**
+     * Returns the key
+     * @return the key as {@code String}
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Returns the key's default value
+     * @return the key's default value as {@code String}
+     */
     public String getDefaultValue() {
         return defaultValue;
     }
