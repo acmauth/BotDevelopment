d7164f26d6c6357558f3b910dc655711ef278076
mavioux <pakmarios@gmail.com>
2021-03-09 18:24:47 +0200

    Stringbuilder Implementation and Emoji Class

--- src/main/java/org/acm/auth/commands/PollCommand.java
+++ src/main/java/org/acm/auth/commands/PollCommand.java
@@ -3,6 +3,7 @@ package org.acm.auth.commands;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.acm.auth.utils.Emoji;
 
 import java.awt.*;
 
@@ -10,37 +11,6 @@ public class PollCommand extends Command{
 
     // static object for empty perms to be used in the default constructor
     private static final Permission[] NO_PERMS = {};
-    private static final String[] reactions = {
-            "\uD83C\uDDE6", // A
-            "\uD83C\uDDE7", // B
-            "\uD83C\uDDE8", // C
-            "\uD83C\uDDE9", // D
-            "\ud83c\uddea", // E
-            "\ud83c\uddeb", // F
-            "\ud83c\uddec", // G
-            "\ud83c\udded", // H
-            "\ud83c\uddee", // I
-            "\ud83c\uddef", // J
-            "\ud83c\uddf0", // K
-            "\ud83c\uddf1", // L
-            "\ud83c\uddf2", // M
-            "\ud83c\uddf3", // N
-            "\ud83c\uddf4", // O
-            "\ud83c\uddf5", // P
-            "\ud83c\uddf6", // Q
-            "\ud83c\uddf7", // R
-            "\ud83c\uddf8", // S
-            "\ud83c\uddf9", // T
-            "\ud83c\uddfa", // U
-            "\ud83c\uddfb", // V
-            "\ud83c\uddfc", // W
-            "\ud83c\uddfd", // X
-            "\ud83c\uddfe", // Y
-            "\ud83c\uddff", // Z
-
-
-
-    };
 
     public PollCommand() {
         super(
@@ -74,8 +44,9 @@ public class PollCommand extends Command{
         eb.setColor(Color.CYAN);
 
         // When the above bug is fixed it should be changed from 1 to length
+        StringBuilder sb = new StringBuilder();
         for (int i = 2; i < args.length; i++) {
-            options += reactions[i-2] + "\t" + args[i] + "\n";
+            options = sb.append(Emoji.REGIONAL_INDICATORS[i-2]).append("\t").append(args[i]).append("\n").toString();
         }
 
         eb.setDescription(options);
@@ -83,7 +54,7 @@ public class PollCommand extends Command{
         int length = args.length - 2;
         event.getChannel().sendMessage(eb.build()).queue(message -> {
             for (int i = 0; i < length; i++) {
-                message.addReaction(reactions[i]).queue();
+                message.addReaction(Emoji.REGIONAL_INDICATORS[i]).queue();
             }
         });
 
---
+++ src/main/java/org/acm/auth/utils/Emoji.java
@@ -0,0 +1,32 @@
+package org.acm.auth.utils;
+
+public class Emoji {
+    public static final String[]  REGIONAL_INDICATORS = {
+            "\uD83C\uDDE6", // A
+            "\uD83C\uDDE7", // B
+            "\uD83C\uDDE8", // C
+            "\uD83C\uDDE9", // D
+            "\ud83c\uddea", // E
+            "\ud83c\uddeb", // F
+            "\ud83c\uddec", // G
+            "\ud83c\udded", // H
+            "\ud83c\uddee", // I
+            "\ud83c\uddef", // J
+            "\ud83c\uddf0", // K
+            "\ud83c\uddf1", // L
+            "\ud83c\uddf2", // M
+            "\ud83c\uddf3", // N
+            "\ud83c\uddf4", // O
+            "\ud83c\uddf5", // P
+            "\ud83c\uddf6", // Q
+            "\ud83c\uddf7", // R
+            "\ud83c\uddf8", // S
+            "\ud83c\uddf9", // T
+            "\ud83c\uddfa", // U
+            "\ud83c\uddfb", // V
+            "\ud83c\uddfc", // W
+            "\ud83c\uddfd", // X
+            "\ud83c\uddfe", // Y
+            "\ud83c\uddff", // Z
+    };
+}
