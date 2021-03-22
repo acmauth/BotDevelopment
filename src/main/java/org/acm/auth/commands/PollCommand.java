77f372ec2ce046f598ccae21e35457820b1dc952
mavioux <pakmarios@gmail.com>
2021-03-22 21:41:20 +0200

    Pattern matcher and delete author message

--- src/main/java/org/acm/auth/commands/PollCommand.java
+++ src/main/java/org/acm/auth/commands/PollCommand.java
@@ -6,11 +6,15 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.acm.auth.utils.Emoji;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PollCommand extends Command{
 
     // static object for empty perms to be used in the default constructor
     private static final Permission[] NO_PERMS = {};
+    private static final Permission[] BOT_PERMS = {Permission.MESSAGE_EMBED_LINKS, Permission.MESSAGE_MANAGE};
 
     public PollCommand() {
         super(
@@ -21,20 +25,26 @@ public class PollCommand extends Command{
                 new String[] {"vote"},
                 3,
                 20, // Maximum Number of Reactions is 20, and it is kinda overkill to have more than 20 options
-                "-poll Question Option1 Option2 ... OptionN",
-                NO_PERMS,
+                "\"Question\" \"Option1\" \"Option2\" ... \"OptionN\"",
+                BOT_PERMS,
                 NO_PERMS
         );
     }
 
     @Override
-    public void invoke(MessageReceivedEvent event, String[] args) {
-        String argsString = String.join(" ", args);
-        // adds an empty string as first and last element, it should be fixed in the future
-        args = argsString.split("(\" )?\"");
+    public void invoke(MessageReceivedEvent event, String[] msg_args) {
+        String argsString = String.join(" ", msg_args);
+        System.out.println(argsString);
+        Pattern p = Pattern.compile("\"([^\"]+)\"");
+        Matcher m = p.matcher(argsString);
+        ArrayList<String> args = new ArrayList<String>();
+        while (m.find()) {
+            String tempArg = m.group();
+            args.add(tempArg.substring(1, tempArg.length() - 1));
+        }
 
-        // When the above bug is fixed it should be changed to args[0]
-        String question = args[1];
+
+        String question = args.get(0);
         String options = "";
 
         // Create the EmbedBuilder instance
@@ -43,15 +53,20 @@ public class PollCommand extends Command{
         eb.setTitle(question, null);
         eb.setColor(Color.CYAN);
 
-        // When the above bug is fixed it should be changed from 1 to length
+        // REGIONAL_INDICATORS[i-1] since we start the loop from 1 and not 0
         StringBuilder sb = new StringBuilder();
-        for (int i = 2; i < args.length; i++) {
-            options = sb.append(Emoji.REGIONAL_INDICATORS[i-2]).append("\t").append(args[i]).append("\n").toString();
+        for (int i = 1; i < args.size(); i++) {
+            sb.append(Emoji.REGIONAL_INDICATORS[i-1]).append("\t").append(args.get(i)).append("\n").toString();
         }
+        options = sb.toString();
 
         eb.setDescription(options);
 
-        int length = args.length - 2;
+        // Delete the message that asked for the poll
+        event.getMessage().delete().queue();
+
+        // length = number of answers so we reduce 1 that corresponds to the question
+        int length = args.size() - 1;
         event.getChannel().sendMessage(eb.build()).queue(message -> {
             for (int i = 0; i < length; i++) {
                 message.addReaction(Emoji.REGIONAL_INDICATORS[i]).queue();
