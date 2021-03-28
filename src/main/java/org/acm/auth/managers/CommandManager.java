68646589ee25db262c5a203b9065999e7074729d
dinos <papakosk@csd.auth.gr>
2021-03-28 18:25:49 +0300

    Fix bug with permission checking for channels

--- src/main/java/org/acm/auth/managers/CommandManager.java
+++ src/main/java/org/acm/auth/managers/CommandManager.java
@@ -60,10 +60,10 @@ public class CommandManager extends ListenerAdapter  {
     private boolean isMissingPerms(Member member, Permission[] permissions, TextChannel tc) {
         return !Arrays.stream(permissions)
                 .allMatch(p -> { // check if the following condition is TRUE for ALL items in the array
-                    return p.isGuild()                      // is the permission a guild-level one?
-                            ? member.hasPermission(p)       // if so (e.g. BAN_MEMBERS), does the user have it globally?
-                            : member.hasPermission(tc, p);  // if not (e.g. MANAGE_MESSAGES), does the user have it in
-                                                            //                                this exact text channel?
+                    return p.isChannel()                    // is the permission a channel-level one?
+                            ? member.hasPermission(tc, p)   // if so (e.g. MANAGE_MESSAGES), does the user have it in
+                                                            //                               this exact text channel?
+                            : member.hasPermission(p);      // if not (e.g. BAN_MEMBERS), does the user have it globally?
                 });
     }
 
